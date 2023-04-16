d0a01390cfe7bd31c325f00b42b50bafdc2537ea
Georg <georg.junemann@web.de>
2023-04-16 15:21:31 +0200

    Added trafficcontrol from github

---
+++ Node/src/main/java/com/github/krextouch/rtds/node/service/trafficcontrol/Coordinate.java
@@ -0,0 +1,22 @@
+package com.github.krextouch.rtds.node.service.trafficcontrol;
+
+/**
+ * simple coordinate class used
+ * */
+public class Coordinate {
+    short x = -1;
+    short y = -1;
+
+    public Coordinate(short x, short y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public short getX() {
+        return x;
+    }
+
+    public short getY() {
+        return y;
+    }
+}
---
+++ Node/src/main/java/com/github/krextouch/rtds/node/service/trafficcontrol/MovementNotPossible.java
@@ -0,0 +1,7 @@
+package com.github.krextouch.rtds.node.service.trafficcontrol;
+
+public class MovementNotPossible extends Exception {
+    public MovementNotPossible(String message) {
+        super(message);
+    }
+}
