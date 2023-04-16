8b8c5ae0b419b94d25c1db24b839012dea1e5e6e
Georg <georg.junemann@web.de>
2023-04-16 19:05:53 +0200

    Removing unnecessary files

--- Node/src/main/java/com/github/krextouch/rtds/node/service/trafficcontrol/MovementNotPossible.java
+++
@@ -1,7 +0,0 @@
-package com.github.krextouch.rtds.node.service.trafficcontrol;
-
-public class MovementNotPossible extends Exception {
-    public MovementNotPossible(String message) {
-        super(message);
-    }
-}
