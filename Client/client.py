17c38ca14142eb72755bd1407edb1778369392e8
Georg <georg.junemann@web.de>
2023-04-17 00:09:57 +0200

    Finished Client.py

--- Client/client.py
+++ Client/client.py
@@ -9,32 +9,26 @@ class Client:
 
     def __init__(self, max_x, max_y, debug=False):
         response = requests.get(url=self.base_url + 'initClient')
-        print(response.text)
-
-        self.cur_pos = [random.randint(0, max_x - 1),
-                        random.randint(0, max_y - 1)]
-        self.dest_pos = [random.randint(0, max_x - 1),
-                         random.randint(0, max_y - 1)]
 
         self.data = {
             'clientId': int(response.text),
-            'curPos': self.cur_pos,
-            'destPos': self.dest_pos
+            'curPos': [random.randint(0, max_x - 1),
+                       random.randint(0, max_y - 1)],
+            'destPos': [random.randint(0, max_x - 1),
+                        random.randint(0, max_y - 1)]
         }
 
         if debug:
-            print(f'curPos: {self.cur_pos}')
-            print(f'destPos: {self.dest_pos}')
+            print(f'curPos: {self.data["curPos"]}')
+            print(f'destPos: {self.data["destPos"]}')
 
-    def start(self):
-        while self.cur_pos[0] is not self.dest_pos[0] or self.cur_pos[1] is not self.dest_pos[1]:
+    def start(self, sleep_timer=0):
+        while not (self.data['curPos'][0] is self.data['destPos'][0]
+                   and self.data['curPos'][1] is self.data['destPos'][1]):
             response = requests.post(url=self.base_url + 'move', json=self.data, headers={})
             self.data['curPos'] = response.json()
-            time.sleep(1)
+            print(response.text)
+            if sleep_timer:
+                time.sleep(sleep_timer)
 
-        print(f'Reached goal: {self.cur_pos}, {self.dest_pos}')
-
-
-test = Client(30, 30, True)
-
-test.start()
+        print(f'Reached goal: {self.data["curPos"]}, {self.data["destPos"]}')
