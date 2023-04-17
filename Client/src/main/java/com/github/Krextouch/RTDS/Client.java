279f7f1546b2560629157a0eac750df8609f857f
Georg <georg.junemann@web.de>
2023-04-17 21:48:27 +0200

    WIP move request

--- Client/src/main/java/com/github/Krextouch/RTDS/Client.java
+++ Client/src/main/java/com/github/Krextouch/RTDS/Client.java
@@ -1,18 +1,78 @@
 package com.github.Krextouch.RTDS;
 
+import java.io.IOException;
 import java.net.URI;
+import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+import org.json.JSONObject;
 
 public class Client {
 
     private final String baseUrl = "http://localhost:8443/api/v1/";
+    private final short clientId;
+    private short[] curPos;
+    private short[] destPos;
 
-    public Client() {
+    HttpClient httpClient = HttpClient.newBuilder()
+            .version(HttpClient.Version.HTTP_2)
+            .build();
+
+    public Client(short maxX, short maxY) throws IOException, InterruptedException {
+        //Get clientId
         HttpRequest request = HttpRequest.newBuilder()
                 .GET()
                 .uri(URI.create(baseUrl + "initClient"))
                 .build();
 
-        System.out.println("---" + request + "---");
+        HttpResponse<String> response = null;
+        response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
+        clientId = Short.parseShort(response.body());
+
+        Random rand = new Random(System.currentTimeMillis());
+        curPos = new short[]{
+                (short) rand.nextInt(maxX),
+                (short) rand.nextInt(maxY)
+        };
+        destPos = new short[]{
+                (short) rand.nextInt(maxX),
+                (short) rand.nextInt(maxY)
+        };
+    }
+
+    public void move(float sleepTimer) {
+
+
+        JSONObject json = new JSONObject();
+        json.put("clientId", clientId);
+        json.put("curPos", curPos);
+        json.put("destPos", destPos);
+
+        System.out.println(json.toString());
+
+        try {
+            HttpRequest request = null;
+            request = HttpRequest.newBuilder()
+                    .POST(HttpRequest.BodyPublishers.ofString(json.toString()))
+                    .uri(URI.create(baseUrl + "move"))
+                    .header("Content-Type", "application/json")
+                    .header("Accept", "application/json")
+                    .build();
+
+            HttpResponse<String> response = null;
+            response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
+            System.out.println(response);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+//        while(!(curPos[0] == destPos[0] && curPos[1] == destPos[1])) {
+//
+//        }
     }
 }
--- Client/src/main/java/com/github/Krextouch/RTDS/ClientApplication.java
+++ Client/src/main/java/com/github/Krextouch/RTDS/ClientApplication.java
@@ -1,7 +1,10 @@
 package com.github.Krextouch.RTDS;
 
+import java.io.IOException;
+
 public class ClientApplication {
-    public static void main(String[] args) {
-        new Client();
+    public static void main(String[] args) throws IOException, InterruptedException {
+        Client client = new Client((short) 100, (short) 100);
+        client.move(0);
     }
 }
