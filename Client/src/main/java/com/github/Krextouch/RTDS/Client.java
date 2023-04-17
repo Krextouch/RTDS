f565007a8793b80e2def72a96848b3745fac5ece
Georg <georg.junemann@web.de>
2023-04-17 23:14:13 +0200

    Finished move requests

--- Client/src/main/java/com/github/Krextouch/RTDS/Client.java
+++ Client/src/main/java/com/github/Krextouch/RTDS/Client.java
@@ -1,30 +1,20 @@
 package com.github.Krextouch.RTDS;
 
+import com.google.gson.Gson;
+
 import java.io.IOException;
-import java.io.UnsupportedEncodingException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Random;
-
-import com.google.gson.Gson;
-import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.methods.CloseableHttpResponse;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.entity.StringEntity;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClientBuilder;
-import org.json.JSONObject;
 
 public class Client {
 
     private final String baseUrl = "http://localhost:8443/api/v1/";
     private final short clientId;
     private short[] curPos;
-    private short[] destPos;
+    private final short[] destPos;
 
     HttpClient httpClient = HttpClient.newBuilder()
             .version(HttpClient.Version.HTTP_2)
@@ -37,7 +27,7 @@ public class Client {
                 .uri(URI.create(baseUrl + "initClient"))
                 .build();
 
-        HttpResponse<String> response = null;
+        HttpResponse<String> response;
         response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
         clientId = Short.parseShort(response.body());
 
@@ -52,50 +42,39 @@ public class Client {
         };
     }
 
-    public void move(float sleepTimer) {
+    public void move(boolean debug) {
+        Gson gson = new Gson();
 
+        while (!(curPos[0] == destPos[0] && curPos[1] == destPos[1])) {
+            try {
+                HttpRequest request;
+                request = HttpRequest.newBuilder()
+                        .POST(HttpRequest.BodyPublishers.ofString(gson.toJson(new Move(clientId, curPos, destPos))))
+                        .uri(URI.create(baseUrl + "move"))
+                        .header("Content-Type", "application/json")
+                        .header("Accept", "application/json")
+                        .build();
+                HttpResponse<String> response;
+                response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
+                curPos = extractValues(response.body());
 
-        JSONObject json = new JSONObject();
-        json.put("clientId", clientId);
-        json.put("curPos", curPos);
-        json.put("destPos", destPos);
-
-        System.out.println(json.toString());
-        String jsonPlain = "{\"clientId:\"14, \"curPos:\" [3,3], \"destPos:\" [4,4]}";
-
-        try {
-            Gson gson = new Gson();
-            CloseableHttpClient httpClient = HttpClientBuilder.create().build();
-            HttpPost post = new HttpPost(baseUrl + "move");
-            StringEntity postingString = new StringEntity(gson.toJson(new Move(clientId, curPos, destPos)));
-            post.setEntity(postingString);
-            post.setHeader("Content-type", "application/json");
-            CloseableHttpResponse  response = httpClient.execute(post);
-            System.out.println(response.toString());
-        } catch (IOException e) {
-            throw new RuntimeException(e);
+                if (debug) {
+                    System.out.println("CurrentPos: " + curPos[0] + ":" + curPos[1]);
+                    System.out.println("DestPos: " + destPos[0] + ":" + destPos[1]);
+                    System.out.println("----------------------");
+                }
+            } catch (IOException | InterruptedException e) {
+                throw new RuntimeException(e);
+            }
         }
+    }
 
-//        try {
-//            HttpRequest request = null;
-//            request = HttpRequest.newBuilder()
-//                    .POST()
-//                    .uri(URI.create(baseUrl + "move"))
-//                    .header("Content-Type", "application/json")
-//                    .header("Accept", "application/json")
-//                    .build();
-//
-//            HttpResponse<String> response = null;
-//            response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
-//            System.out.println(response);
-//        } catch (IOException e) {
-//            throw new RuntimeException(e);
-//        } catch (InterruptedException e) {
-//            throw new RuntimeException(e);
-//        }
-
-//        while(!(curPos[0] == destPos[0] && curPos[1] == destPos[1])) {
-//
-//        }
+    private short[] extractValues(String responseString) {
+        String shortendString = responseString.substring(1, responseString.length() - 1);
+        
+        return new short[]{
+                Short.parseShort(shortendString.split(",")[0]),
+                Short.parseShort(shortendString.split(",")[1])
+        };
     }
 }
--- Client/src/main/java/com/github/Krextouch/RTDS/ClientApplication.java
+++ Client/src/main/java/com/github/Krextouch/RTDS/ClientApplication.java
@@ -1,10 +1,17 @@
 package com.github.Krextouch.RTDS;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 public class ClientApplication {
     public static void main(String[] args) throws IOException, InterruptedException {
-        Client client = new Client((short) 100, (short) 100);
-        client.move(0);
+        Map<String, Short> convertedArgs = new HashMap<>();
+        for (String s : args) {
+            String[] splitArgs = s.split("=");
+            convertedArgs.put(splitArgs[0], Short.valueOf(splitArgs[1]));
+        }
+        Client client = new Client(convertedArgs.get("maxX"), convertedArgs.get("maxY"));
+        client.move(false);
     }
 }
