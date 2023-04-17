0a8c43d091dacb69f53636878db434f13f13d2b0
Kevin <kevinworster@live.de>
2023-04-17 12:43:40 +0200

    finished node app downloading and running

--- Scripts/VirtualBox/initNode.sh
+++ Scripts/VirtualBox/initNode.sh
@@ -8,7 +8,7 @@ wget -O - https://packages.adoptium.net/artifactory/api/gpg/key/public | sudo ap
 echo "deb https://packages.adoptium.net/artifactory/deb $(awk -F= '/^VERSION_CODENAME/{print$2}' /etc/os-release) main" | sudo tee /etc/apt/sources.list.d/adoptium.list
 
 # Add mongoDB repository to apt
-#wget -qO - https://www.mongodb.org/static/pgp/server-6.0.asc | sudo apt-key add -
+get -qO - https://www.mongodb.org/static/pgp/server-6.0.asc | sudo apt-key add -
 #echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/6.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-6.0.list
 sudo apt-get update
 
@@ -16,7 +16,7 @@ sudo apt-get update
 sudo apt install -y temurin-17-jdk
 
 # Install mongoDB
-#sudo apt install -y mongodb-org
+sudo apt install -y mongodb-org
 
 # Create mongoDB dbpath
 sudo mkdir /var/lib/mongodb/configS
@@ -24,7 +24,7 @@ sudo mkdir /var/lib/mongodb/routerS
 sudo mkdir /var/lib/mongodb/shardS
 
 # Run mongod instances
-#sudo mongod --config /vagrant/mongo/config/configServer.config &
+sudo mongod --config /vagrant/mongo/config/configServer.config &
 sleep 5s
 
 # Install gradle
@@ -36,11 +36,15 @@ sudo mkdir /opt/gradle
 sudo unzip -d /opt/gradle gradle-8.1-bin.zip
 export PATH=$PATH:/opt/gradle/gradle-8.1/bin
 
-# Clone github repository, build and run node app
+# Clone github repository
 sudo mkdir /opt/RTDSnode
 cd /opt/RTDSnode
 RTDSbranch="main"
-# TODO:
-#sudo curl -L https://github.com/Krextouch/RTDS/archive/$RTDSbranch -o RTDS.tar.gz
-#sudo tar -xvzf RTDS.tar.gz --strip-components=1 RTDS-$RTDSbranch/Node
-#gradle clean build
+url="https://github.com/Krextouch/RTDS/archive/${RTDSbranch}.tar.gz"
+sudo curl -L "${url}" -o RTDS.tar.gz
+sudo tar -xvzf RTDS.tar.gz --strip-components=1 RTDS-${RTDSbranch}/Node
+sudo rm RTDS.tar.gz
+cd Node
+
+# Build and run gradle node app
+gradle clean build
