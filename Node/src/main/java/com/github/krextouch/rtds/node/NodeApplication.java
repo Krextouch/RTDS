04799e8ec603cd8e7966c094fe1d390c8995e135
Georg <georg.junemann@web.de>
2023-04-30 19:32:09 +0200

    Added comments

--- Node/src/main/java/com/github/krextouch/rtds/node/NodeApplication.java
+++ Node/src/main/java/com/github/krextouch/rtds/node/NodeApplication.java
@@ -3,6 +3,11 @@ package com.github.krextouch.rtds.node;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * spring application that starts the Road Traffic Divert System
+ * all args must be present -> example configuration:
+ * --maxX=1000 --maxY=1000 --maxClientsPerCoordinate=2
+ */
 @SpringBootApplication
 public class NodeApplication {
     public static void main(String[] args) {
